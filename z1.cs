f12352527fa3a8ec29335330cbd40d5753231fcb
Vladimir <mail@myexample.com>
2022-03-06 19:12:37 +0300

    Задача 19

--- z1.cs
+++ z1.cs
@@ -6,13 +6,6 @@ int n5 = number % 10;
 int n2 = (number - n1 * 10000) / 1000;
 int n4 = (number % 100 - n5) / 10;
 
-/*
-Console.WriteLine(n1);
-Console.WriteLine(n2);
-Console.WriteLine(n4);
-Console.WriteLine(n5);
-*/
-
 if(n1 == n5 && n2 == n4)
 {
     Console.WriteLine("Да");
