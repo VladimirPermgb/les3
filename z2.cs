4b1d76a35dc078861c8f393ad22919e26e14f0a3
Vladimir <mail@myexample.com>
2022-03-06 19:44:41 +0300

    Задача 23

--- z2.cs
+++ z2.cs
@@ -1,9 +1,13 @@
 ﻿Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" ");
 
-int n1 = number / 10000;
-int n5 = number % 10;
-int n2 = (number - n1 * 10000) / 1000;
-int n4 = (number % 100 - n5) / 10;
+int count = 1;
 
-// while 
+while (count <= number)
+{
+    Console.Write(count*count*count);
+    Console.Write(", ");
+    count++;
+    
+}
