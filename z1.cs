8cdf805aa04caa2a52866701c4359fa330abe410
Vladimir <mail@myexample.com>
2022-03-06 19:12:21 +0300

    Задача 19

--- z1.cs
+++ z1.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int n1 = number / 10000;
+int n5 = number % 10;
+int n2 = (number - n1 * 10000) / 1000;
+int n4 = (number % 100 - n5) / 10;
+
+/*
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n4);
+Console.WriteLine(n5);
+*/
+
+if(n1 == n5 && n2 == n4)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+    
