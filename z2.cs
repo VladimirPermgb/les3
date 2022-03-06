d1891da526a43943c1829408be50821e55c179f5
Vladimir <mail@myexample.com>
2022-03-06 19:15:46 +0300

    Initial

---
+++ z2.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int n1 = number / 10000;
+int n5 = number % 10;
+int n2 = (number - n1 * 10000) / 1000;
+int n4 = (number % 100 - n5) / 10;
+
+// while 
