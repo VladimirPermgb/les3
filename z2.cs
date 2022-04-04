948b2a4b7a868699f1ee170ca56322cd610a1fc8
VladimirPermgb <100301190+VladimirPermgb@users.noreply.github.com>
2022-04-04 10:49:00 +0300

    Update z2.cs

--- z2.cs
+++ z2.cs
@@ -1,10 +1,18 @@
 // Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(" ");
-
-int count = 1;
-
+Console.WriteLine("Введите количество чисел");
+int kolvo = Convert.ToInt32(Console.ReadLine());
+int j = 0;
+for (int i = 1; i <= kolvo; i++)
+{
+    Console.WriteLine("Введите число " + i);
+    int chislo = Convert.ToInt32(Console.ReadLine());
+    if (chislo > 0)
+    {
+        j = j + 1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine(j + " - Количество чисел больше нуля");
 
 // Задача 2: Пользователь вводит с клавиатуры кол-во строк и столбцов для массива. Вывести массив в виде матрицы, создав "рамку" из единиц, а внутреннюю зону матрицы заполнить нулями.
 
